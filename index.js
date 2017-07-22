108aac1b4642ee7abdd05cddf947f82c5c14dec9
Adam Duncan <adam.duncan@clock.co.uk>
2017-07-22 14:07:32 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = init
+
+var Emitter = require('events').EventEmitter
+  , mqtt = require('mqtt')
+
+function init(callback) {
+  callback(null, 'mqtt', Mqtt)
+}
+
+function Mqtt(automait, logger, config) {
+  Emitter.call(this)
+  this.automait = automait
+  this.logger = logger
+  this.config = config
+  this.client = null
+}
+
+Mqtt.prototype = Object.create(Emitter.prototype)
+
+Mqtt.prototype.init = function () {
+  this.client = mqtt.connect('mqtt://' + this.config.connString)
+
+  this.client.on('connect', function () {
+    this.client.subscribe('#')
+  })
+
+  // this.client.on('message', function (topic, message) {
+  //   console.log(message.toString())
+  // }.bind(this))
+}
+
+Mqtt.prototype.publish = function (topic, message) {
+  this.client.publish(topic, message)
+}
+
+
